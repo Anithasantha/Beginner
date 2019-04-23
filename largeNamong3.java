56926bc2927775e823c593bf96878c1e8f1de7c1
Anithasantha <49860898+Anithasantha@users.noreply.github.com>
2019-04-23 11:08:56 +0530

    Update and rename largeNamong3.py to largeNamong3.java

---
+++ largeNamong3.java
@@ -0,0 +1,20 @@
+#Anitha
+#Beginner 1.5
+import java.io.*;
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    int n1,n2,n3;
+    Scanner sc=new Scanner(System.in);
+    n1=sc.nextInt();
+    n2=sc.nextInt();
+    n3=sc.nextInt();
+    if(n1>=n2 && n1>=n3)
+      System.out.println(n1);
+    else 
+      if(n2>=n3)
+        System.out.println(n2);
+      else
+        System.out.println(n3);
+  }
+}
